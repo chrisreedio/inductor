fc47565e0fc3ba630fc6b141c99a76caa922f6a2
Chris Reed <chris@reedtech.us>
2024-09-06 05:39:24 -0500

    ✨ feat: added PHPVersion.vue component
    Created `PHPVersion.vue` to display the PHP version.

---
+++ resources/js/Components/PHPVersion.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({php_version: String})
+</script>
+<template>
+    <div class="flex gap-x-2 border border-green-500 p-2">
+        PHP Version: {{ php_version }}
+    </div>
+</template>
