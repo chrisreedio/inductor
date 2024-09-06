1a89445cf33f352894630372d46c3e1636f4e387
Chris Reed <chris@reedtech.us>
2024-09-06 16:25:58 -0500

    ✨ feat: added component ID and shared data to VueApplication
    Introduced `component_id` and a `page` array with shared data in `VueApplication`. Refactored the view to display these properties for debugging purposes.

--- resources/views/livewire/vue-application.blade.php
+++ resources/views/livewire/vue-application.blade.php
@@ -1,6 +1,12 @@
-<div id="wirevue-{{ $component_id }}"
-     data-wirevue
-     data-wirevue-id="{{ $component_id }}"
-     data-wirevue-component="{{ $component }}"
-     data-wirevue-props="{{ json_encode(array_merge($page, $props), JSON_UNESCAPED_SLASHES) }}">
+<div>
+{{--    {{ $test_prop }}--}}
+    <h3>{{ $component_id }}</h3>
+    <pre>{!! print_r($page, true) !!}}</pre>
+    <pre>{!! print_r($props, true) !!}}</pre>
+    {{--<div id="wirevue-{{ $component_id }}"--}}
+    {{--     data-wirevue--}}
+    {{--     data-wirevue-id="{{ $component_id }}"--}}
+    {{--     data-wirevue-component="{{ $component }}"--}}
+    {{--     data-wirevue-props="{{ json_encode(array_merge($page ?? [], $props ?? []), JSON_UNESCAPED_SLASHES) }}">--}}
+    {{--</div>--}}
 </div>
--- src/Livewire/VueApplication.php
+++ src/Livewire/VueApplication.php
@@ -6,24 +6,39 @@ use ChrisReedIO\Inductor\Inductor;
 use Illuminate\Support\Str;
 use Livewire\Component;
 
+use function array_walk;
 use function auth;
 use function phpversion;
 
 class VueApplication extends Component
 {
+    public ?string $component_id = null;
+
     public ?string $component = null;
 
     public array $props = [];
 
+    public ?array $page = [
+        'initial_key' => 'initial_value',
+    ];
+
+    public function mount()
+    {
+        $this->component_id = Str::random(8);
+        // $this->component = $component;
+        // $this->props = $props;
+        $sharedData = Inductor::getSharedData();
+        $this->page['php_version'] = phpversion();
+        array_walk($sharedData, function ($value, $key) {
+            $this->page[$key] = $value;
+        });
+    }
+
     public function render()
     {
-        return view('inductor::livewire.vue-application', [
-            'component_id' => Str::random(8),
-            'page' => [
-                // 'user' => auth()->user(),
-                // 'php_version' => phpversion(),
-                ...Inductor::getSharedData(),
-            ],
-        ]);
+        return view('inductor::livewire.vue-application')
+            ->with([
+                'page' => $this->page,
+            ]);
     }
 }
