29c098b59d2029e4d9b6659aa58ef11c769d291c
Chris Reed <chris@reedtech.us>
2024-09-06 17:29:17 -0500

    🔧 fix: uncommented and adjusted Livewire Vue component markup
    Restored and modified the previously commented-out Livewire Vue wrapper component structure.

--- resources/views/livewire/vue-application.blade.php
+++ resources/views/livewire/vue-application.blade.php
@@ -1,19 +1,18 @@
 <div>
-{{--    {{ $test_prop }}--}}
-    This is a Livewire Vue wrapper component. Component: {{ $component }}
-    <div class="flex gap-x-2">
-        <dt>Div ID:</dt>
-        <dd>{{ $component_id }}</dd>
-    </div>
-    <h3>Props</h3>
-    <pre>{{ print_r($props,true) }}</pre>
-    <hr>
-    <h3>Page</h3>
-    <pre>{{ print_r($page, true) }}</pre>
-    {{--<div id="wirevue-{{ $component_id }}"--}}
-    {{--     data-wirevue--}}
-    {{--     data-wirevue-id="{{ $component_id }}"--}}
-    {{--     data-wirevue-component="{{ $component }}"--}}
-    {{--     data-wirevue-props="{{ json_encode(array_merge($page ?? [], $props ?? []), JSON_UNESCAPED_SLASHES) }}">--}}
+    {{--This is a Livewire Vue wrapper component. Component: {{ $component }}--}}
+    {{--<div class="flex gap-x-2">--}}
+    {{--    <dt>Div ID:</dt>--}}
+    {{--    <dd>{{ $component_id }}</dd>--}}
     {{--</div>--}}
+    {{--<h3>Props</h3>--}}
+    {{--<pre>{{ print_r($props,true) }}</pre>--}}
+    {{--<hr>--}}
+    {{--<h3>Page</h3>--}}
+    {{--<pre>{{ print_r($page, true) }}</pre>--}}
+    <div id="wirevue-{{ $component_id }}"
+         data-wirevue
+         data-wirevue-id="{{ $component_id }}"
+         data-wirevue-component="{{ $component }}"
+         data-wirevue-props="{{ json_encode(array_merge($page ?? [], $props ?? []), JSON_UNESCAPED_SLASHES) }}">
+    </div>
 </div>
