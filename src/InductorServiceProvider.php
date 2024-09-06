40f03b757dbe7ca170ff030fe7f23476a1ad9ab6
chrisreedio <chrisreedio@users.noreply.github.com>
2024-09-06 22:29:56 +0000

    Fix styling

--- src/InductorServiceProvider.php
+++ src/InductorServiceProvider.php
@@ -61,9 +61,7 @@ class InductorServiceProvider extends PackageServiceProvider
         $package->hasViewComponents('inductor', \ChrisReedIO\Inductor\Components\VueWrapper::class);
     }
 
-    public function packageRegistered(): void
-    {
-    }
+    public function packageRegistered(): void {}
 
     public function packageBooted(): void
     {
@@ -83,7 +81,7 @@ class InductorServiceProvider extends PackageServiceProvider
 
         // Handle Stubs
         if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__.'/../stubs/') as $file) {
+            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
                 $this->publishes([
                     $file->getRealPath() => base_path("stubs/inductor/{$file->getFilename()}"),
                 ], 'inductor-stubs');
